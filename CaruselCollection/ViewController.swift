6ccb243973468f84d8cbd5a222bffd4780240add
Azonaz <malinina.a@gmail.com>
2025-02-05 13:37:43 +0200

    Initial Commit

---
+++ CaruselCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaruselCollection
+//
+//  Created by Админ on 05/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
