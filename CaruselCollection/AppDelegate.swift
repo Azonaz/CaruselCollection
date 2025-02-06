85958e8f6b80d2248edf0e41454cfd9dbbc5d486
Azonaz <malinina.a@gmail.com>
2025-02-06 09:58:27 +0200

    Added readme

--- CaruselCollection/AppDelegate.swift
+++ CaruselCollection/AppDelegate.swift
@@ -3,14 +3,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
-        func application(
-            _ application: UIApplication,
-            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-        ) -> Bool {
-            window = UIWindow(frame: UIScreen.main.bounds) // ВАЖНО: задать frame
-            window?.rootViewController = CarouselViewController()
-            window?.makeKeyAndVisible()
-            return true
-        }
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = CarouselViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
 }
